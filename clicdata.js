f352f9bb84d04e337e2784bf18ecbdfde8da9fc4
Jay <jay@niquecraft.net>
2015-06-15 17:29:05 +1000

    Update clicdata.js

--- clicdata.js
+++ clicdata.js
@@ -3,15 +3,15 @@ var page = require('webpage').create(),
 
 page.open('https://xo.clicdata.com/v/ezaGSsfLXHoP', function() {
 	setTimeout(function() {
-       	var values = page.evaluate(function() {
-       		return JSON.stringify({
-       			xos_online: document.getElementsByClassName('mainText')[0].innerHTML,
-       			hours_used: document.getElementsByClassName('mainText')[1].innerHTML,
-       			xos_shipped: document.getElementsByClassName('mainText')[2].innerHTML,
-       			schools: document.getElementsByClassName('mainText')[3].innerHTML,
-       		});
+		var values = page.evaluate(function() {
+			return JSON.stringify({
+				xos_online: document.getElementsByClassName('mainText')[0].innerHTML,
+				hours_used: document.getElementsByClassName('mainText')[1].innerHTML,
+				xos_shipped: document.getElementsByClassName('mainText')[2].innerHTML,
+				schools: document.getElementsByClassName('mainText')[3].innerHTML,
+			});
 		});
 		fs.write('data.json', values, 'w');
 		phantom.exit();
-   	}, 15000);
+	}, 15000);
 });
